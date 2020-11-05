41eb9911a8336450e028af9193d3a73b34954650
tekschool <ghulam.qasimi555@gmail.com>
2020-11-04 21:21:48 -0500

    new file added

--- src/test/java/testNG/concepts/TestClass.java
+++ src/test/java/testNG/concepts/TestClass.java
@@ -1,5 +1,12 @@
 package testNG.concepts;
 
+import org.junit.Test;
+
 public class TestClass {
 
+	
+	 @Test 
+	 public void test() {
+		 System.out.println("this class is pushed to repo");
+	 }
 }
